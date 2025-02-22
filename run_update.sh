ca896dc31752664f88fd8176e33b2949eab0e108
navigator2601 <navigator.vip14@gmsil.com>
2025-02-22 22:25:18 +0200

    Prepare for AWS Elastic Beanstalk deployment

--- run_update.sh
+++ run_update.sh
@@ -3,4 +3,4 @@ git commit -m "Оновлення додатку та Dockerfile для забе
 git push origin main
 docker build -t gcr.io/kondiki/telegram-bot .
 docker push gcr.io/kondiki/telegram-bot
-gcloud run deploy telegram-bot --image gcr.io/kondiki/telegram-bot --platform managed --region europe-west3 --set-env-vars TELEGRAM_BOT_TOKEN=8177185933:AAGvnm0JmuTxucr8VqU0nzGd4WrNkn5VHpU,DB_URL="postgresql://neondb_owner:npg_dhwrDX6O1keB@ep-round-star-a9r38wl3-pooler.gwc.azure.neon.tech/neondb"
+gcloud run deploy telegram-bot --image gcr.io/kondiki/telegram-bot --platform managed --region europe-west3 --set-env-vars TELEGRAM_BOT_TOKEN=8177185933:AAGvnm0JmuTxucr8VqU0nzGd4WrNkn5VHpU
